1fd6bff1608272166ceb00936f07394b8d440c8e
Seun Ogundipe <sogundipe@gmail.com>
2025-01-30 05:37:03 +0000

    No-Ticket: Added custom deploy step

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,18 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 const run = () => {
+    // Get input values for the deployment target
+    const bucketName = core.getInput('bucket-name', {required: true})
+    const sourceFolder = core.getInput('dist-folder', {required: true})
+    const region = core.getInput('bucket-region', {required: true})
+
+
+
+    // Use input to call AWS api to upload to bucket
+    exec.exec(`aws s3 sync ${sourceFolder} s3://${bucketName} --region ${region}`)
+
     core.notice('Hello from my custom Javascript action')
 }
 
